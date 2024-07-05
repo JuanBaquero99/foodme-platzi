4a6ecb07b8a6b449bbe791a6a7f0f5eda6b7eb94
JuanBaquero99 <baquerojuan99@gmail.com>
2024-07-04 20:10:03 -0500

    Update section help and Who We Are

--- server/index.js
+++ server/index.js
@@ -2,26 +2,23 @@
 const newrelic = require('newrelic');
 
 const express = require('express');
-const logger = require('pino')(); // Setting up logger using Pino
-const morgan = require('morgan'); // Logging middleware for Express
-const bodyParser = require('body-parser'); // Middleware for parsing JSON request bodies
+const logger = require('pino')();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const fs = require('fs');
 const open = require('open');
 
-const RestaurantRecord = require('./model').Restaurant; // Importing RestaurantRecord class from a local file
-const MemoryStorage = require('./storage').Memory; // Importing in-memory storage implementation from a local file
+const RestaurantRecord = require('./model').Restaurant;
+const MemoryStorage = require('./storage').Memory;
 
-// Constants for API routes
 const API_URL = '/api/restaurant';
 const API_URL_ID = API_URL + '/:id';
 const API_URL_ORDER = '/api/order';
 
-// Utility function to remove 'menuItems' property from a restaurant
 var removeMenuItems = function(restaurant) {
   var clone = {};
 
-  // Cloning the object while omitting the 'menuItems' property
   Object.getOwnPropertyNames(restaurant).forEach(function(key) {
     if (key !== 'menuItems') {
       clone[key] = restaurant[key];
@@ -31,33 +28,28 @@ var removeMenuItems = function(restaurant) {
   return clone;
 };
 
-// Exported main function to initialize and configure the server
 exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
-  var app = express(); // Creating an instance of Express
-  var storage = new MemoryStorage(); // Creating an instance of in-memory storage
+  var app = express();
+  var storage = new MemoryStorage();
 
-  // Middleware to log all HTTP requests
+  // log requests
   app.use(morgan('combined'));
 
-  // Middleware to serve static files from the specified directory
+  // serve static files for demo client
   app.use(express.static(STATIC_DIR));
 
-  // Middleware to parse JSON request bodies
+  // create application/json parser
   var jsonParser = bodyParser.json();
 
-  // RESTful API routes
-
-  // Endpoint to get all restaurants
+  // API
   app.get(API_URL, function(req, res, next) {
     res.send(200, storage.getAll().map(removeMenuItems));
   });
 
-  // Endpoint to add a new restaurant
   app.post(API_URL, function(req, res, next) {
     var restaurant = new RestaurantRecord(req.body);
     var errors = [];
 
-    // Validation of the restaurant and storing if successful
     if (restaurant.validate(errors)) {
       storage.add(restaurant);
       return res.send(201, restaurant);
@@ -66,14 +58,10 @@ exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
     return res.send(400, {error: errors});
   });
 
-  // Endpoint to process an order (order creation)
   app.post(API_URL_ORDER, jsonParser, function(req, res, next) {
     logger.info(req.body, 'checkout');
-
-    /*************************************
-    /*         Custom attributes         *
-    /*************************************/
-    // Example of logging custom attributes with New Relic
+     
+    
     var order = req.body;
     var itemCount = 0;
     var orderTotal = 0;
@@ -82,20 +70,17 @@ exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
       orderTotal += item.price * item.qty;
     });
     
-    
     newrelic.addCustomAttributes({
       'customer': order.deliverTo.name,
       'restaurant': order.restaurant.name,
       'itemCount': itemCount,
       'orderTotal': orderTotal
     });
+    
 
-
-    // Responding with the generated order ID
     return res.send(201, { orderId: Date.now()});
   });
 
-  // Endpoint to get a restaurant by its ID
   app.get(API_URL_ID, function(req, res, next) {
     var restaurant = storage.getById(req.params.id);
     if (restaurant) {
@@ -104,18 +89,15 @@ exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
     return res.send(400, {error: 'No restaurant with id "' + req.params.id + '"!'});
   });
 
-  // Endpoint to update a restaurant by its ID
   app.put(API_URL_ID, function(req, res, next) {
     var restaurant = storage.getById(req.params.id);
     var errors = [];
 
-    // Updating existing restaurant
     if (restaurant) {
       restaurant.update(req.body);
       return res.send(200, restaurant);
     }
 
-    // Creating a new restaurant if one doesn't exist with that ID
     restaurant = new RestaurantRecord(req.body);
     if (restaurant.validate(errors)) {
       storage.add(restaurant);
@@ -125,44 +107,34 @@ exports.start = function(PORT, STATIC_DIR, DATA_FILE, TEST_DIR) {
     return res.send(400, {error: errors});
   });
 
-  // Endpoint to delete a restaurant by its ID
-  app.delete(API_URL_ID, function(req, res, next) {
+  app.del(API_URL_ID, function(req, res, next) {
     if (storage.deleteById(req.params.id)) {
       return res.send(204, null);
     }
     return res.send(400, {error: 'No restaurant with id "' + req.params.id + '"!'});
   });
 
-  // Initial read of data from JSON file and server startup
+  // read the data from json and start the server
   fs.readFile(DATA_FILE, function(err, data) {
-    if (err) {
-      console.error('Error reading data file:', err);
-      return;
-    }
-
-    // Parsing JSON data and storing in in-memory storage
     JSON.parse(data).forEach(function(restaurant) {
       storage.add(new RestaurantRecord(restaurant));
     });
 
-    // Starting server on specified port
     app.listen(PORT, function() {
       open('http://localhost:' + PORT + '/');
       // console.log('Go to http://localhost:' + PORT + '/');
     });
   });
 
-  // Signal handler to save restaurant data to JSON file before exiting
+  // Windows and Node.js before 0.8.9 would crash
+  // https://github.com/joyent/node/issues/1553
   try {
     process.on('SIGINT', function() {
-      fs.writeFile(DATA_FILE, JSON.stringify(storage.getAll()), function(err) {
-        if (err) {
-          console.error('Error saving data:', err);
-        }
+      // save the storage back to the json file
+      fs.writeFile(DATA_FILE, JSON.stringify(storage.getAll()), function() {
         process.exit(0);
       });
     });
-  } catch (e) {
-    console.error('Error setting up SIGINT handler:', e);
-  }
+  } catch (e) {}
+
 };
--- server/model.js
+++ server/model.js
@@ -1,14 +1,11 @@
-// Utility function to generate an ID from a name by converting to lowercase and removing non-word characters
 var idFromName = function(name) {
   return name && name.toLowerCase().replace(/\W/g, '');
 };
 
-// Utility function to check if a value is a string
 var isString = function(value) {
   return typeof value === 'string';
 };
 
-// Object mapping short day names to their corresponding numeric representation
 var DAYS = {
   Su: 0,
   Mo: 1,
@@ -19,46 +16,39 @@ var DAYS = {
   Sa: 6
 };
 
-// Function to parse a comma-separated string of days into an array of numeric day representations
 var parseDays = function(str) {
   return str.split(',').map(function(day) {
     return DAYS[day];
   });
 };
 
-// Constructor function for Restaurant objects
-var Restaurant = function(data) {
-  // Default values
-  this.days = [1, 2, 3, 4, 5, 6]; // Default days of operation
-  this.menuItems = []; // Empty array for menu items
-  this.price = 0; // Default price
-  this.rating = 0; // Default rating
 
-  // Update restaurant object with provided data
+var Restaurant = function(data) {
+  // defaults
+  this.days = [1, 2, 3, 4, 5, 6];
+  this.menuItems = [];
+  this.price = 0;
+  this.rating = 0;
+
   this.update(data);
 
-  // Generate an ID from the name if not already provided
   this.id = this.id || idFromName(this.name);
 };
 
-// Method to update Restaurant object with new data
 Restaurant.prototype.update = function(data) {
   Object.keys(data).forEach(function(key) {
-    // Convert price and rating to integers if they are strings
-    if ((key === 'price' || key === 'rating') && isString(data[key])) {
+    if (key === 'price' || key === 'rating' && isString(data[key])) {
       this[key] = parseInt(data[key], 10);
     } else {
       this[key] = data[key];
     }
   }, this);
 
-  // Map menu items to MenuItem objects
   this.menuItems = this.menuItems.map(function(data) {
     return new MenuItem(data);
   });
 };
 
-// Method to validate Restaurant object
 Restaurant.prototype.validate = function(errors) {
   if (!this.name) {
     errors.push('Invalid: "name" is a mandatory field!');
@@ -67,7 +57,6 @@ Restaurant.prototype.validate = function(errors) {
   return errors.length === 0;
 };
 
-// Static method to create a Restaurant object from an array of data
 Restaurant.fromArray = function(data) {
   return new Restaurant({
     id: data[1],
@@ -83,13 +72,12 @@ Restaurant.fromArray = function(data) {
   });
 };
 
-// Constructor function for MenuItem objects
+
 var MenuItem = function(data) {
   this.name = data.name;
   this.price = data.price;
 };
 
-// Static method to create a MenuItem object from an array of data
 MenuItem.fromArray = function(data) {
   return new MenuItem({
     name: data[1],
@@ -97,6 +85,5 @@ MenuItem.fromArray = function(data) {
   });
 };
 
-// Exporting Restaurant and MenuItem constructors
 exports.Restaurant = Restaurant;
 exports.MenuItem = MenuItem;
